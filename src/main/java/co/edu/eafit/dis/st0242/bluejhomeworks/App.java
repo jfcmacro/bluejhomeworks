f51aa139b2e03c148b503b67894eb76b06e16604
Juan Francisco Cardona McCormick <jfcmacro@gmail.com>
2015-02-20 07:41:43 -0500

    Start of the project

---
+++ src/main/java/co/edu/eafit/dis/st0242/bluejhomeworks/App.java
@@ -0,0 +1,13 @@
+package co.edu.eafit.dis.st0242.bluejhomeworks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
